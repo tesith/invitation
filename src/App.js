13ee87e2170f30003b707861e85f0fb929b1075b
tesith <tesith@naver.com>
2024-09-01 21:21:06 +0900

    오류 수정 배포 준비

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Gallery from "./components/gallery";
 import Image from "./components/image.js";
